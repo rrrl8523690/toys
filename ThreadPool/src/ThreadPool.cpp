4891b04eff2f838af6852b45eaaa8c09d2f720b2
rrrl8523690 <552189774@qq.com>
2017-09-15 22:08:27 -0700

    Add ThreadPool class. Modify folder structure.

---
+++ ThreadPool/src/ThreadPool.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Handa Wang on 9/15/17.
+//
+
+#include "ThreadPool.h"
---
+++ ThreadPool/src/ThreadPool.h
@@ -0,0 +1,14 @@
+//
+// Created by Handa Wang on 9/15/17.
+//
+
+#ifndef THREADPOOL_THREADPOOL_H
+#define THREADPOOL_THREADPOOL_H
+
+
+class ThreadPool {
+
+};
+
+
+#endif //THREADPOOL_THREADPOOL_H
